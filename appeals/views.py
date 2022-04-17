16d0c357223e255d2d672f1f67d3007d113d38da
Oybek Yuldashev <oybekyuldashov54@gmail.com>
2022-04-17 23:47:34 +0500

    asdas

--- appeals/views.py
+++ appeals/views.py
@@ -7,35 +7,52 @@ from .models import *
 
 
 def home(request):
+    all_appeals_cnt = Appeal.objects.all().count()
+    process_appeals_cnt = Appeal.objects.filter(appeal_status='process').count()
+    rejected_appeals_cnt = Appeal.objects.filter(appeal_status='rejected').count()
+    completed_appeals_cnt = Appeal.objects.filter(appeal_status='completed').count()
+    new_appeals_cnt = Appeal.objects.filter(appeal_status='new').count()
+    
+    
     applicants_panel_info = Applicants_panel.objects.first()
     socials = Social.objects.all()
     appealForm = AppealForm()
     answers = False
-
-    if request.POST.get('answer_code'):
-        code = request.POST.get('answer_code')
-        appeal = Appeal.objects.get(code=code)
-        answers = Answer.objects.get(appeal=appeal)
-
-        # messages.success(request, f'{answers}, {answers.text}')
-        return redirect('home')
-        
-        
-    elif request.POST:
-
-        appealForm = AppealForm(request.POST, request.FILES)
-        if appealForm.is_valid():
-            obj = appealForm.save(commit=False)
-            obj.save()
-
-            messages.success(request, f"Bu sizning javobni korish uchun parolingiz, Ishonchli joyga saqlab qoying!!!{obj.code}")
-            return redirect('home')
-        
+    
     context = {
         'appealForm': appealForm,
         'applicants_panel_info': applicants_panel_info,
         'socials': socials,
+
+        
+        'all_appeals_cnt': all_appeals_cnt,
+        'process_appeals_cnt': process_appeals_cnt,
+        'rejected_appeals_cnt': rejected_appeals_cnt,
+        'completed_appeals_cnt': completed_appeals_cnt,
+        'new_appeals_cnt': new_appeals_cnt,
     }
+
+    if request.POST:
+        if request.POST.get('answer_code'):
+
+            code = request.POST.get('answer_code')
+            appeal = Appeal.objects.get(code=code)
+            answers = Answer.objects.get(appeal=appeal)
+
+            # messages.success(request, f'{answers}, {answers.text}')
+            return redirect('home')
+        
+        
+        else:
+            appealForm = AppealForm(request.POST, request.FILES)
+
+            if appealForm.is_valid():
+                obj = appealForm.save(commit=False)
+                obj.save()
+
+                # messages.success(request, f"Bu sizning javobni korish uchun parolingiz, Ishonchli joyga saqlab qoying!!!{obj.code}")
+                return redirect('home')
+        
     
     if answers:
         context['answers'] = answers
